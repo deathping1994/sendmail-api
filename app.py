b0ddcf0e1a7a81211f91850ba1e3200558bcf5c6
Gaurav Shukla <gshukla66@gmail.com>
2015-10-14 22:22:01 +0530

    now takes app id in mailer url

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask,request
 from flask.ext.bcrypt import Bcrypt
 from flask.ext.pymongo import PyMongo
 from flask import jsonify
+from bson.objectid import ObjectId
 import requests
 app = Flask("FREEMAIL")
 mongo=PyMongo(app)
@@ -17,15 +18,14 @@ def register():
         data=request.get_json(force=True)
         appdetail['email']=data['email']
         appdetail['password']=data['password']
-        mongo.db.registeredapp.insert({"appemail":appdetail['email'],"password":appdetail['password'],
+        res=mongo.db.registeredapp.insert({"appemail":appdetail['email'],"password":appdetail['password'],
         "pwhash":bcrypt.generate_password_hash(appdetail['password'])})
         gmail_user =data['email']
         gmail_pwd = data['password']
         FROM = data['email']
         TO = data['email']
         SUBJECT = "FREEMAIL Registration"
-        TEXT = "hi your app has been registered and your auth token is %s" %(bcrypt.generate_password_hash(data['email']+str(datetime.datetime.now())))
-
+        TEXT = "hi your app has been registered and your token is %s\n and your appid is %s" %(bcrypt.generate_password_hash(data['email']+str(datetime.datetime.now())),str(res))
         message = "FROM:%s\nTO:%s\nSUBJECT:%s\n%s" % (FROM,TO,SUBJECT,TEXT)
         try:
             server = smtplib.SMTP("smtp.gmail.com", 587)
@@ -43,28 +43,31 @@ def register():
         return jsonify(error="something went wrong maybe you supplied wrong details"),500
 
 
-@app.route('/mailer/<authtoken>',methods=["GET","POST"])
-def send_mail(authtoken):
+@app.route('/mailer/<appid>',methods=["GET","POST"])
+def send_mail(appid):
     try:
-        res=mongo.db.registeredapp.find_one({"pwhash":authtoken})
+        res=mongo.db.registeredapp.find_one({"_id": ObjectId(appid)})
         if res is not None:
             data=request.get_json(force=True)
-            gmail_user =res['appemail']
-            gmail_pwd = res['password']
-            FROM = res["appemail"]
-            TO = data['to']
-            SUBJECT = data['subject']
-            TEXT = data['message']
-            credits = "Email sent from FREEMAIL :developed by GAURAV SHUKLA (github handle deathping1994)" \
-                    "\nDisclaimer: Developer does not hold any responsibility as to how this service may be used"
-            message = "FROM:%s\nTO:%s\nSUBJECT:%s\n%s\n \n%s" % (FROM,TO,SUBJECT,TEXT,credits)
-            server = smtplib.SMTP("smtp.gmail.com", 587)
-            server.ehlo()
-            server.starttls()
-            server.login(gmail_user, gmail_pwd)
-            server.sendmail(FROM, TO, message)
-            server.close()
-            return jsonify(success='successfully sent the e-mail'),200
+            if data['token']==res['pwhash']:
+                gmail_user =res['appemail']
+                gmail_pwd = res['password']
+                FROM = res["appemail"]
+                TO = data['to']
+                SUBJECT = data['subject']
+                TEXT = data['message']
+                credits = "Email sent from FREEMAIL :developed by GAURAV SHUKLA (github handle deathping1994)" \
+                        "\nDisclaimer: Developer does not hold any responsibility as to how this service may be used"
+                message = "FROM:%s\nTO:%s\nSUBJECT:%s\n%s\n \n%s" % (FROM,TO,SUBJECT,TEXT,credits)
+                server = smtplib.SMTP("smtp.gmail.com", 587)
+                server.ehlo()
+                server.starttls()
+                server.login(gmail_user, gmail_pwd)
+                server.sendmail(FROM, TO, message)
+                server.close()
+                return jsonify(success='successfully sent the e-mail'),200
+            else:
+                return jsonify(error="You supplied wrong token."),403
         else:
             return jsonify(error="Your app is not registered to use this service"),500
     except Exception as e:
